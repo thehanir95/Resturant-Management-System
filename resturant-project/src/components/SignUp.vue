c57456529586b0bcdb13284c91c2a33e78cfa673
thehanir95 <thehanicis95@gmail.com>
2021-08-21 15:06:57 +0530

    Newly

---
+++ resturant-project/src/components/SignUp.vue
@@ -0,0 +1,41 @@
+<template>
+<img class="logo" src="./../assets/1.jpg">
+    <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter Name" />
+        <input type="text" placeholder="Enter Email" />
+        <input type="password" placeholder="Enter password" />
+        <button>Sign Up</button>
+    </div>
+</template>
+<script>
+export default{
+    name : 'SignUp'
+}
+</script>
+
+<style>
+.logo{
+    width: 100px;
+}
+
+.register input{
+    width: 300px;
+    height: 50px;
+    padding-left: 200px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+
+.register button{
+    width: 120px;
+    height: 50px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: white;
+    cursor: pointer;
+}
+</style>
