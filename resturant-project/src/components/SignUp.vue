3a10a2d57acc7b17cc0744ef917b8ac81fae56fc
thehanir95 <thehanicis95@gmail.com>
2021-08-22 07:55:50 +0530

    "Update"

--- resturant-project/src/components/SignUp.vue
+++ resturant-project/src/components/SignUp.vue
@@ -2,15 +2,40 @@
 <img class="logo" src="./../assets/1.jpg">
     <h1>Sign Up</h1>
     <div class="register">
-        <input type="text" placeholder="Enter Name" />
-        <input type="text" placeholder="Enter Email" />
-        <input type="password" placeholder="Enter password" />
-        <button>Sign Up</button>
+        <input type="text" v-model="name" placeholder="Enter Name" />
+        <input type="text" v-model="email" placeholder="Enter Email" />
+        <input type="password" v-model="password" placeholder="Enter password" />
+        <button v-on:click="signup">Sign Up</button>
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default{
-    name : 'SignUp'
+    name : 'SignUp',
+    data()
+    {
+        return {
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+       async signup()
+        {
+           let result = await axios.post("http://localhost:3000/users",{
+               email:this.email,
+               password:this.password,
+               name:this.name
+           });
+           console.warn(result);
+           if(result.status==201)
+           {
+               alert("signup done");
+               localStorage.setItem("use-info",JSON.stringify(result.data))
+           }
+        }
+    }
 }
 </script>
 
