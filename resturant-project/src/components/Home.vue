5cd5017d3c1e1e801f6b998ff236b80b8ca34871
thehanir95 <thehanicis95@gmail.com>
2021-08-23 16:07:01 +0530

    add Home.vue

---
+++ resturant-project/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello User, Welcome Home page</h1>
+</template>
+
+<script>
+export default{
+    name:'Home',
+    mounted()
+    {
+        let user=localStorage.getItem('user-info');
+        if(!user)
+        {
+          this.$router.push({name:'SignUp'})  
+        }
+    }
+}
+</script>
