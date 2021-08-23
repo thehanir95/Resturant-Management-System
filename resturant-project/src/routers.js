f32b35626e74918e1cb17b2a42e8612694b678ff
thehanir95 <thehanicis95@gmail.com>
2021-08-23 16:08:43 +0530

    add routers.js

---
+++ resturant-project/src/routers.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './components/Home.vue'
+import SignUp from './components/SignUp.vue'
+import Login from './components/Login.vue'
+
+const routes=[
+    {
+        name:'Home',
+        component:Home,
+        path:'/'
+
+    }, 
+    { 
+        name:'SignUp',
+        component:SignUp,
+        path:'/sign-up'
+    },
+    { 
+        name:'Login',
+        component:Login,
+        path:'/login'
+    }
+
+];
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes,
+});
+
+export default router;
