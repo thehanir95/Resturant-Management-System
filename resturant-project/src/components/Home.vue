cdc377b672c4545ab226a95ade2ab8ebed43290f
thehanir95 <thehanicis95@gmail.com>
2021-08-30 22:00:31 +0530

    Update

--- resturant-project/src/components/Home.vue
+++ resturant-project/src/components/Home.vue
@@ -1,21 +1,38 @@
 <template>
 <Header />
-    <h1>Hello {{name}} , Welcome Home page</h1>
+    <h1>Hello {{name}}, Welcome Home page</h1>
+    <table border="1">
+        <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Contact</td>
+            <td>Address</td>
+        </tr>
+
+        <tr v-for="item in resturants" :key="item.id">
+        <td>{{item.id}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.contact}}</td>
+        <td>{{item.address}}</td>
+        </tr>
+    </table>
 </template>
 
 <script>
 import Header from './Header.vue'
+import axios from 'axios'
 export default{
     name:'Home',
     data(){
         return {
-            name:''
+            name:'',
+            resturants:[],
         }
     },
     components:{
         Header
     },
-    mounted()
+    async mounted()
     {
         let user=localStorage.getItem('user-info');
         this.name= JSON.parse(user).name;
@@ -23,6 +40,16 @@ export default{
         {
           this.$router.push({name:'SignUp'})  
         }
+        let result = await axios.get("http://localhost:3000/resturants");
+        console.warn(result);
+        this.resturants= result.data;
     }
 }
 </script>
+
+<style>
+td{
+    width: 160px;
+    height: 40;
+}
+</style>
