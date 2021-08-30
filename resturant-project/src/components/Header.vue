33d75fcfb400fd321743d8c79111a824d51a3531
thehanir95 <thehanicis95@gmail.com>
2021-08-30 19:54:45 +0530

    Add Header.vue

---
+++ resturant-project/src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/add">Add Resturant</router-link>
+        <router-link to="/update">Update Resturant</router-link>
+        <a v-on:click="logout" href="#">Logout</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Header' ,
+    methods:{
+        logout()
+        {
+            localStorage.clear();
+            this.$router.push({name:'Login'})
+        }
+    }
+}
+</script>
+<style>
+.nav{
+  background-color: #333;  
+  overflow: hidden;
+}
+
+.nav a{
+    float: left;
+    color: white;
+    padding: 14px 10px;
+    text-align: center;
+    font-size: 17px;
+    text-decoration: none;
+    margin-right: 5px;
+}
+.nav a:hover{
+    background: #ddd;
+    color: #333;
+}
+</style>
