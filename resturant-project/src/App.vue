38bdef96b419e2d53630c72774eec63b00839c7f
thehanir95 <thehanicis95@gmail.com>
2021-08-31 11:17:28 +0530

    update

--- resturant-project/src/App.vue
+++ resturant-project/src/App.vue
@@ -26,7 +26,7 @@ body{
     width: 100px;
 }
 
-.register input{
+.register input, .login input, .add input{
     width: 300px;
     height: 50px;
     padding-left: 200px;
@@ -37,27 +37,7 @@ body{
     border: 1px solid skyblue;
 }
 
-.login input{
-    width: 300px;
-    height: 50px;
-    padding-left: 200px;
-    display: block;
-    margin-bottom: 30px;
-    margin-right: auto;
-    margin-left: auto;
-    border: 1px solid skyblue;
-}
-
-.register button{
-    width: 120px;
-    height: 50px;
-    border: 1px solid skyblue;
-    background: skyblue;
-    color: white;
-    cursor: pointer;
-}
-
-.login button{
+.register button, .login button, .add button{
     width: 120px;
     height: 50px;
     border: 1px solid skyblue;
--- resturant-project/src/components/AddResturant.vue
+++ resturant-project/src/components/AddResturant.vue
@@ -1,15 +1,48 @@
 <template>
 <Header />
     <h1>Hello User, Welcome AddResturant page</h1>
+    <form class="add">
+        <input type="text" name="name" placeholder="Enter Name" v-model="resturants.name">
+        <input type="text" name="address" placeholder="Enter Address" v-model="resturants.address">
+        <input type="text" name="contact" placeholder="Enter Contact" v-model="resturants.contact">
+        <button type="button" v-on:click="addResturant">Add New Resturant</button>
+    </form>
 </template>
 
 <script>
 import Header from './Header.vue'
+import axios from 'axios'
 export default{
     name:'AddResturant',
     components:{
         Header
     },
+    data(){
+        return {
+            resturants: {
+                name : '',
+                address : '',
+                contact : ''
+            }
+        }
+    },
+    methods:{
+       async addResturant()
+        {
+            console.warn(this.resturants)
+            const result = await axios.post("http://localhost:3000/resturants",{
+                name :this.resturants.name,
+                address : this.resturants.address,
+                contact : this.resturants.contact
+            });
+            if(result.status==201)
+            {
+                this.$router.push({name:'Home'})  
+            }
+            console.warn("result",result)  
+            }
+    },
+
     mounted()
     {
         let user=localStorage.getItem('user-info');
