f3103bcd2d73349d5e8a12e0fe3c3e64cdc6564e
thehanir95 <thehanicis95@gmail.com>
2021-09-08 06:25:48 +0530

    Update Resturant.vue

--- resturant-project/src/components/UpdateResturant.vue
+++ resturant-project/src/components/UpdateResturant.vue
@@ -1,6 +1,12 @@
 <template>
 <Header />
     <h1>Hello User, Welcome UpdateResturant page</h1>
+     <form class="add">
+        <input type="text" name="name" placeholder="Enter Name" v-model="resturants.name">
+        <input type="text" name="address" placeholder="Enter Address" v-model="resturants.address">
+        <input type="text" name="contact" placeholder="Enter Contact" v-model="resturants.contact">
+        <button type="button" v-on:click="addResturant">Add New Resturant</button>
+    </form>
 </template>
 
 <script>
@@ -10,6 +16,17 @@ export default{
     components:{
         Header
     },
+
+    data(){
+        return {
+            resturants: {
+                name : '',
+                address : '',
+                contact : ''
+            }
+        }
+    },
+
     mounted()
     {
         let user=localStorage.getItem('user-info');
